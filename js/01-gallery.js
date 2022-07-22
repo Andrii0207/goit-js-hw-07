bd1957f71d7dd13df213a52cb0668253c9e0d31a
Andrii Popov <popov.andrey80@gmail.com>
2022-07-22 23:42:00 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function onCreateListImages(item) {
+  return galleryItems
+    .map(
+      item => `<div class="gallery__item">
+      <a class="gallery__link" href = "${item.original}">
+      <img class="gallery__image" src="${item.preview}"
+      data-source="${item.original}"
+      alt="Image description"/>
+      </a>
+      </div>`,
+    )
+    .join('');
+  console.log(galleryItems);
+}
+
+const onCreateGallery = onCreateListImages(galleryItems);
+console.log(onCreateGallery);
