fa260a4e6d2cd571592e1deb38a197a8f250f5dc
Andrii Popov <popov.andrey80@gmail.com>
2022-07-24 22:48:09 +0300

    tast 01 almost done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,20 +3,43 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-function onCreateListImages(item) {
-  return galleryItems
-    .map(
-      item => `<div class="gallery__item">
-      <a class="gallery__link" href = "${item.original}">
-      <img class="gallery__image" src="${item.preview}"
-      data-source="${item.original}"
-      alt="Image description"/>
-      </a>
-      </div>`,
-    )
-    .join('');
-  console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+// console.log(galleryEl);
+
+function onCreateListImages(galleryItems) {
+  // return galleryItems
+  //   .map(
+  //     item => `<div class="gallery__item">
+  //     <a class="gallery__link" href = "${item.original}">
+  //     <img class="gallery__image" src="${item.preview}"
+  //     data-source="${item.original}"
+  //     alt="${item.description}"/>
+  //     </a>
+  //     </div>`,
+  //   )
+  //   .join('');
+  return galleryItems.reduce(
+    (acc, img) =>
+      acc +
+      `<div class="gallery"><a class="gallery__link" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}" data-source="${img.original}"/></a></div>`,
+    '',
+  );
 }
 
 const onCreateGallery = onCreateListImages(galleryItems);
-console.log(onCreateGallery);
+// console.log(onCreateGallery);
+
+galleryEl.innerHTML = onCreateGallery;
+
+galleryEl.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return console.log('Its not a picture');
+  }
+  const originalSizeImg = event.target.dataset.source;
+  basicLightbox.create(`<img src="${originalSizeImg}">`).show();
+}
