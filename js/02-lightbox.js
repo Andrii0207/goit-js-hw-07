bb485b51c5fcc122783c2d8ea4f961bfde2339bd
Andrii Popov <popov.andrey80@gmail.com>
2022-07-26 22:04:18 +0300

    task-02 done!

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,14 +9,10 @@ function onCreateListImg(galleryItems) {
   return galleryItems.reduce(
     (acc, item) =>
       acc +
-      `<div class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-            <img class="gallery__image"
-            src="${item.preview}"
-            data-source="${item.original}"
-            alt="${item.description}"/>
-        </a>
-  </div>`,
+      `<a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" 
+              alt="${item.description}}" />
+      </a>`,
     '',
   );
 }
@@ -25,26 +21,8 @@ const onCreateGallery = onCreateListImg(galleryItems);
 
 galleryEl.innerHTML = onCreateGallery;
 
-galleryEl.addEventListener('click', onOpenModal);
-
-function onOpenModal(event) {
-  event.preventDefault();
-
-  if (!event.target.classList.contains('gallery__image')) {
-    return console.log('Its not a picture');
-  }
-
-  const originalSizeImg = event.target.dataset.source;
-  const instance = basicLightbox.create(`
-    <img src="${originalSizeImg}"/>`);
-
-  instance.show();
-
-  window.addEventListener('keydown', onEscClick);
-
-  function onEscClick(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
